8b37f56235c5aa8ffae5e5df3f3c60f82ce2c31d
Milena Brum <milena.brum@astropay.com>
2022-03-17 14:16:46 -0300

    add footer

--- client/src/components/Layout/Footer/Footer.tsx
+++ client/src/components/Layout/Footer/Footer.tsx
@@ -1,10 +1,17 @@
 import { Box } from '@mui/material';
 import React from 'react';
+import styles from './Footer.module.css';
 
 const Footer: React.FC<{}> = () => (
-  <div>
-    <Box />
-  </div>
+  <Box className={styles.root}>
+    <p>
+      Made with ❤ by
+      <a href="https://github.com/milebrum" target="_blank" rel="noopener noreferrer">
+        {' '}
+        Milena Brum • 2022
+      </a>
+    </p>
+  </Box>
 );
 
 export default Footer;
