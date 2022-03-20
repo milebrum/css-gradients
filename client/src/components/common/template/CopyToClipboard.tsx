ee9fcbfc66440589680119c8face83e0159752c5
Milena Brum <milena.brum@astropay.com>
2022-03-20 18:25:20 -0300

    get css in rgba according to output format

--- client/src/components/common/template/CopyToClipboard.tsx
+++ client/src/components/common/template/CopyToClipboard.tsx
@@ -1,15 +1,32 @@
+import { hexToRgb } from '@material-ui/core';
 import RoutePaths from '../../../config/constants/route-paths';
-import { CopyToClipboardType, StyleType } from './types';
+import { CopyToClipboardType, OutputFormatType, StyleType } from './types';
 
 const CopyToClipboard = (toCopy: string, gradient: any) => {
+  const rgbAColours: string[] = [];
+
+  if (gradient.gradient.outputFormat === OutputFormatType.RGBA) {
+    gradient.gradient.colours.forEach((colour: string) => {
+      rgbAColours.push(hexToRgb(colour));
+    });
+  }
+
   switch (toCopy) {
     case CopyToClipboardType.CSS:
       navigator.clipboard.writeText(
-        `/* Created with https://www.css-gradients.herokuapp.com */
-        background: ${gradient.gradient.colours[0]};
-        background: -webkit-${gradient.gradient.style}(${gradient.gradient.direction.replace(/to /g, '')}, ${gradient.gradient.colours});
-        background: -moz-${gradient.gradient.style}(${gradient.gradient.direction.replace(/to /g, '')}, ${gradient.gradient.colours});
-        background: ${gradient.gradient.style}(${gradient.gradient.style === StyleType.LINEAR ? gradient.gradient.direction : gradient.gradient.direction.replace(/to /g, 'at ')}, ${gradient.gradient.colours});`,
+        gradient.gradient.outputFormat === OutputFormatType.RGBA ? (
+          `/* Created with https://www.css-gradients.herokuapp.com */
+          background: ${rgbAColours[0]};
+          background: -webkit-${gradient.gradient.style}(${gradient.gradient.direction.replace(/to /g, '')}, ${rgbAColours});
+          background: -moz-${gradient.gradient.style}(${gradient.gradient.direction.replace(/to /g, '')}, ${rgbAColours});
+          background: ${gradient.gradient.style}(${gradient.gradient.style === StyleType.LINEAR ? gradient.gradient.direction : gradient.gradient.direction.replace(/to /g, 'at ')}, ${rgbAColours});`
+        ) : (
+          `/* Created with https://www.css-gradients.herokuapp.com */
+            background: ${gradient.gradient.colours[0]};
+            background: -webkit-${gradient.gradient.style}(${gradient.gradient.direction.replace(/to /g, '')}, ${gradient.gradient.colours});
+            background: -moz-${gradient.gradient.style}(${gradient.gradient.direction.replace(/to /g, '')}, ${gradient.gradient.colours});
+            background: ${gradient.gradient.style}(${gradient.gradient.style === StyleType.LINEAR ? gradient.gradient.direction : gradient.gradient.direction.replace(/to /g, 'at ')}, ${gradient.gradient.colours});`
+        ),
       );
       break;
     case CopyToClipboardType.SHARE_LINK:
