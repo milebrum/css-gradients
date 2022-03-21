ebd5b4bbb837bc06f27abe2c32b63f9bb0524455
Milena Brum <milena.brum@astropay.com>
2022-03-21 12:42:58 -0300

    fix deploy

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,8 +14,6 @@ app.use(bodyParser.json());
 
 // add router in the Express app.
 app.use("/", router);
-
-router.use(express.static(path.resolve(__dirname, '../client/build')));
 
 const templates = [];
 
